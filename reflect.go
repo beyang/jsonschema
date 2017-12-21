91b8a5fd28e95097817d8539e684d54716ca3c8c
Beyang Liu <beyang@sourcegraph.com>
2017-12-20 18:12:09 -0800

    read description field

--- reflect.go
+++ reflect.go
@@ -11,9 +11,9 @@ import (
 	"net"
 	"net/url"
 	"reflect"
+	"strconv"
 	"strings"
 	"time"
-	"strconv"
 )
 
 // Version is the JSON Schema version.
@@ -253,7 +253,6 @@ func (r *Reflector) reflectStruct(definitions Definitions, t reflect.Type) *Type
 	definitions[t.Name()] = st
 	r.reflectStructFields(st, definitions, t)
 
-
 	return &Type{
 		Version: Version,
 		Ref:     "#/definitions/" + t.Name(),
@@ -277,7 +276,9 @@ func (r *Reflector) reflectStructFields(st *Type, definitions Definitions, t ref
 		if name == "" {
 			continue
 		}
+		description := r.reflectFieldDescription(f)
 		property := r.reflectTypeToSchema(definitions, f.Type)
+		property.Description = description
 		property.structKeywordsFromTags(f)
 		st.Properties[name] = property
 		if required {
@@ -286,9 +287,9 @@ func (r *Reflector) reflectStructFields(st *Type, definitions Definitions, t ref
 	}
 }
 
-func (t *Type) structKeywordsFromTags(f reflect.StructField){
+func (t *Type) structKeywordsFromTags(f reflect.StructField) {
 	tags := strings.Split(f.Tag.Get("jsonschema"), ",")
-	switch t.Type{
+	switch t.Type {
 	case "string":
 		t.stringKeywords(tags)
 	case "number":
@@ -302,53 +303,53 @@ func (t *Type) structKeywordsFromTags(f reflect.StructField){
 
 // read struct tags for string type keyworks
 func (t *Type) stringKeywords(tags []string) {
-    for _, tag := range tags{
-        nameValue := strings.Split(tag, "=")
-        if len(nameValue) == 2{
-	        name, val := nameValue[0], nameValue[1]
-	        switch name{
-	            case "minLength":
-	            	i, _ := strconv.Atoi(val)
-	                t.MinLength = i
-	            case "maxLength":
-	            	i, _ := strconv.Atoi(val)
-	                t.MaxLength = i
-	            case "format":
-	                switch val{
-	                case "date-time", "email", "hostname", "ipv4", "ipv6", "uri":
-	                    t.Format = val
-	                    break
-	                }
-	        }
-    	}
-    }
+	for _, tag := range tags {
+		nameValue := strings.Split(tag, "=")
+		if len(nameValue) == 2 {
+			name, val := nameValue[0], nameValue[1]
+			switch name {
+			case "minLength":
+				i, _ := strconv.Atoi(val)
+				t.MinLength = i
+			case "maxLength":
+				i, _ := strconv.Atoi(val)
+				t.MaxLength = i
+			case "format":
+				switch val {
+				case "date-time", "email", "hostname", "ipv4", "ipv6", "uri":
+					t.Format = val
+					break
+				}
+			}
+		}
+	}
 }
 
 // read struct tags for numberic type keyworks
 func (t *Type) numbericKeywords(tags []string) {
-    for _, tag := range tags{
-        nameValue := strings.Split(tag, "=")
-        if len(nameValue) == 2{
-        	name, val := nameValue[0], nameValue[1]
-	        switch name{
-	            case "multipleOf":
-	            	i, _ := strconv.Atoi(val)
-	                t.MultipleOf = i
-	            case "minimum":
-	            	i, _ := strconv.Atoi(val)
-	                t.Minimum = i
-	            case "maximum":
-	            	i, _ := strconv.Atoi(val)
-	                t.Maximum = i
-	            case "exclusiveMaximum":
-	            	b, _ := strconv.ParseBool(val)
-	                t.ExclusiveMaximum = b
-	            case "exclusiveMinimum":
-	            	b, _ := strconv.ParseBool(val)
-	                t.ExclusiveMinimum = b
-	        }
-        }
-    }
+	for _, tag := range tags {
+		nameValue := strings.Split(tag, "=")
+		if len(nameValue) == 2 {
+			name, val := nameValue[0], nameValue[1]
+			switch name {
+			case "multipleOf":
+				i, _ := strconv.Atoi(val)
+				t.MultipleOf = i
+			case "minimum":
+				i, _ := strconv.Atoi(val)
+				t.Minimum = i
+			case "maximum":
+				i, _ := strconv.Atoi(val)
+				t.Maximum = i
+			case "exclusiveMaximum":
+				b, _ := strconv.ParseBool(val)
+				t.ExclusiveMaximum = b
+			case "exclusiveMinimum":
+				b, _ := strconv.ParseBool(val)
+				t.ExclusiveMinimum = b
+			}
+		}
+	}
 }
 
 // read struct tags for object type keyworks
@@ -369,22 +370,22 @@ func (t *Type) numbericKeywords(tags []string) {
 
 // read struct tags for array type keyworks
 func (t *Type) arrayKeywords(tags []string) {
-    for _, tag := range tags{
-        nameValue := strings.Split(tag, "=")
-        if len(nameValue) == 2{
-	        name, val := nameValue[0], nameValue[1]
-	        switch name{
-	            case "minItems":
-	            	i, _ := strconv.Atoi(val)
-	                t.MinItems = i
-	            case "maxItems":
-	            	i, _ := strconv.Atoi(val)
-	                t.MaxItems = i
-	            case "uniqueItems":
-	            	t.UniqueItems = true
-	        }
-    	}
-    }
+	for _, tag := range tags {
+		nameValue := strings.Split(tag, "=")
+		if len(nameValue) == 2 {
+			name, val := nameValue[0], nameValue[1]
+			switch name {
+			case "minItems":
+				i, _ := strconv.Atoi(val)
+				t.MinItems = i
+			case "maxItems":
+				i, _ := strconv.Atoi(val)
+				t.MaxItems = i
+			case "uniqueItems":
+				t.UniqueItems = true
+			}
+		}
+	}
 }
 
 func requiredFromJSONTags(tags []string) bool {
@@ -448,4 +449,8 @@ func (r *Reflector) reflectFieldName(f reflect.StructField) (string, bool) {
 	}
 
 	return name, required
+}
+
+func (r *Reflector) reflectFieldDescription(f reflect.StructField) string {
+	return f.Tag.Get("description")
 }
